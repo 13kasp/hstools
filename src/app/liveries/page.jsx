a72303a28e46be228e7f7aa11f3c5d2fcbef3094
kasp <kasparas.streckis@gmail.com>
2025-06-14 23:19:54 +0300

    force dynamic

--- src/app/liveries/page.jsx
+++ src/app/liveries/page.jsx
@@ -1,6 +1,8 @@
 import { db } from "@/db";
 import LiveriesClientWrapper from "../components/LiveriesClientWrapper";
 
+export const dynamic = "force-dynamic";
+
 export default async function LiveriesPage() {
   const liveries = await db.liveries.findMany();
 
