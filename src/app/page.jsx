197c6f4f06298d47866f317e0085a52fe0d73f2f
kasp <kasparas.streckis@gmail.com>
2025-06-14 22:10:36 +0300

    no scroll on home lolz

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="relative w-screen h-screen">
+    <div className="relative w-screen h-screen overflow-hidden">
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col gap-4">
           <div className="text-6xl">HSTOOLS</div>
